23152042c4c9e96fc2c80441c654582cc3bceb28
rachanne22 <rachel.sickle@gmail.com>
2022-03-19 23:57:12 -0400

    tts class code

---
+++ src/main/java/com/tts/myAwesomeProject/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.tts.myAwesomeProject.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+}
+
+
